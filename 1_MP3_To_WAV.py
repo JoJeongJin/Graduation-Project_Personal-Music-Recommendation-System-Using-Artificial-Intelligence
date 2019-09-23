99996c3ae89dc391df9d09ebee05d4bb7a1b523d
JoJeongJin <apjojeongjin@gmail.com>
2019-09-23 23:03:53 +0900

    더 쉽게 어떤 파일이 WAV로 변환중인지 확인할 수 있음

--- 1_MP3_To_WAV.py
+++ 1_MP3_To_WAV.py
@@ -9,7 +9,8 @@ for _list in music_type:
     file_list_dir = mp3_path_directory + _list +"/"
     file_list = os.listdir(file_list_dir)
     print(file_list)
+    print("파일 개수: "+ str(len(file_list)))
     for item in file_list:
         sound = AudioSegment.from_mp3(file_list_dir+item)
-        print("./Music/WAV/"+_list+"/")
+        print("./Music/WAV/"+_list+"/"+item)
         sound.export("./Music/WAV/"+_list+"/"+item+".wav", format="wav")
