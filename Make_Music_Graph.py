de8a7172ca482fdcb123ec742cecddb908028477
JoJeongJin <apjojeongjin@gmail.com>
2019-10-06 23:32:41 +0900

    음악 이미지를 20퍼센트 정도를 자동으로 테스트로 뽑게 함

--- Make_Music_Graph.py
+++ Make_Music_Graph.py
@@ -6,43 +6,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1번째 방법
-def make_graph(img_path,filepath,filename):
+def make_graph(img_path,filepath,filename,music_type):
 
     wr = wave.open(filepath, 'r')
     sz = wr.getframerate()
     q = 120  # time window to analyze in seconds
     c = 1  # number of time windows to process
     sf = 0.8  # signal scale factor
+    index = 0
 
     for num in range(c):
-        print('Processing from {} to {} s'.format(num*q, (num+1)*q))
-        avgf = np.zeros(int(sz/2+1))
-        snd = np.array([])
-        # The sound signal for q seconds is concatenated. The fft over that
-        # period is averaged to average out noise.
-        for j in range(q):
-            da = np.fromstring(wr.readframes(sz), dtype=np.int16)
-            left, right = da[0::2]*sf, da[1::2]*sf
-            lf, rf = abs(np.fft.rfft(left)), abs(np.fft.rfft(right))
-            snd = np.concatenate((snd, (left+right)/2))
-            avgf += (lf+rf)/2
-        avgf /= q
-        # Plot both the signal and frequencies.
-        plt.figure(1)
-        a = plt.subplot(211)  # signal
-        r = 2**16/2
-        a.set_ylim([-r, r])
-        a.set_xlabel('time [s]')
-        a.set_ylabel('signal [-]')
-        x = np.arange(44100*q)/44100
-        plt.plot(x, snd)
-        b = plt.subplot(212)  # frequencies
-        b.set_xscale('log')
-        b.set_xlabel('frequency [Hz]')
-        b.set_ylabel('|amplitude|')
-        plt.plot(abs(avgf))
-        plt.savefig(img_path+filename+'.png')
-        plt.clf()
+
+        index = index + 1
+        try:
+            print('Processing from {} to {} s'.format(num * q, (num + 1) * q))
+            avgf = np.zeros(int(sz / 2 + 1))
+            snd = np.array([])
+            # The sound signal for q seconds is concatenated. The fft over that
+            # period is averaged to average out noise.
+            for j in range(q):
+                da = np.fromstring(wr.readframes(sz), dtype=np.int16)
+                left, right = da[0::2] * sf, da[1::2] * sf
+                lf, rf = abs(np.fft.rfft(left)), abs(np.fft.rfft(right))
+                snd = np.concatenate((snd, (left + right) / 2))
+                avgf += (lf + rf) / 2
+            avgf /= q
+            # Plot both the signal and frequencies.
+            plt.figure(1)
+            a = plt.subplot(211)  # signal
+            r = 2 ** 16 / 2
+            a.set_ylim([-r, r])
+            a.set_xlabel('time [s]')
+            a.set_ylabel('signal [-]')
+            x = np.arange(44100 * q) / 44100
+            plt.plot(x, snd)
+            b = plt.subplot(212)  # frequencies
+            b.set_xscale('log')
+            b.set_xlabel('frequency [Hz]')
+            b.set_ylabel('|amplitude|')
+            plt.plot(abs(avgf))
+
+            if(index%5==0):
+                plt.savefig(img_path + filename + '.png')
+            else:
+                plt.savefig("./test_set/"+ music_type + "/" + filename + '.png')
+            plt.clf()
+        except:
+            print("오류 발생 : " + filename)
+
 # 2
 # import matplotlib.pyplot as plt
 # import numpy as np
